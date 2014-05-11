1db0021820c1061d6450b0df7a75e0f12d4ee5ad
skyhat <skyhat@mail.ru>
2014-05-11 23:56:34 +0300

    better than nothing

---
+++ Client/Controllers/HouseController.cs
@@ -0,0 +1,134 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Client.Models;
+
+namespace Client.Controllers
+{
+    public class HouseController : Controller
+    {
+        private readonly DbEntities _db = new DbEntities();
+
+        // GET: /House/
+        public ActionResult Index()
+        {
+            var houses = _db.Houses.Include(h => h.Type);
+            return View(houses.ToList());
+        }
+
+        // GET: /House/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            House house = _db.Houses.Find(id);
+            if (house == null)
+            {
+                return HttpNotFound();
+            }
+            return View(house);
+        }
+
+        // GET: /House/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.TypeId = new SelectList(_db.Types, "Id", "Type1");
+            return View();
+        }
+
+        // POST: /House/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include="Id,RoomCount,TypeId,Price,AvailabilityDate,Owner,Photo")] House house)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Houses.Add(house);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TypeId = new SelectList(_db.Types, "Id", "Type1", house.TypeId);
+            return View(house);
+        }
+
+        // GET: /House/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            House house = _db.Houses.Find(id);
+            if (house == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TypeId = new SelectList(_db.Types, "Id", "Type1", house.TypeId);
+            return View(house);
+        }
+
+        // POST: /House/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include="Id,RoomCount,TypeId,Price,AvailabilityDate,Owner,Photo")] House house)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(house).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TypeId = new SelectList(_db.Types, "Id", "Type1", house.TypeId);
+            return View(house);
+        }
+
+        // GET: /House/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            House house = _db.Houses.Find(id);
+            if (house == null)
+            {
+                return HttpNotFound();
+            }
+            return View(house);
+        }
+
+        // POST: /House/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            House house = _db.Houses.Find(id);
+            _db.Houses.Remove(house);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Client/Controllers/TypeController.cs
@@ -0,0 +1,126 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Client.Models;
+
+namespace Client.Controllers
+{
+    [Authorize]
+    public class TypeController : Controller
+    {
+        private readonly DbEntities _db = new DbEntities();
+
+        // GET: /Type/
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(_db.Types.ToList());
+        }
+
+        // GET: /Type/Details/5
+        [AllowAnonymous]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var type = _db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // GET: /Type/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Type/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Type1")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Types.Add(type);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(type);
+        }
+
+        // GET: /Type/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var type = _db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: /Type/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Type1")] Type type)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(type).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(type);
+        }
+
+        // GET: /Type/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var type = _db.Types.Find(id);
+            if (type == null)
+            {
+                return HttpNotFound();
+            }
+            return View(type);
+        }
+
+        // POST: /Type/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var type = _db.Types.Find(id);
+            _db.Types.Remove(type);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
