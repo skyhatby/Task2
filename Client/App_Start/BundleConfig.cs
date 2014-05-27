439018e10e13aa7e8554a8a842750517fdf46495
skyhat <skyhat@mail.ru>
2014-05-28 00:36:50 +0300

    Added HttpModule and some fixes

--- Client/App_Start/BundleConfig.cs
+++ Client/App_Start/BundleConfig.cs
@@ -9,8 +9,7 @@ namespace Client
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,7 +21,8 @@ namespace Client
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ Client/CustomModule/CustModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Web;
+
+namespace Client.CustomModule
+{
+    public class CustModule : IHttpModule
+    {
+        private MyEventHandler _eventHandler;
+        private static readonly ConcurrentDictionary<string, int> Hits = new ConcurrentDictionary<string, int>();
+        public void Dispose()
+        { }
+        public delegate void MyEventHandler(Object s, EventArgs e);
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += OnBeginRequest;
+        }
+        public event MyEventHandler MyEvent
+        {
+            add { _eventHandler += value; }
+            remove { _eventHandler -= value; }
+        }
+        public void OnBeginRequest(Object s, EventArgs e)
+        {
+            var app = s as HttpApplication;
+            if (app != null)
+            {
+                int num;
+                if (!Hits.TryGetValue(app.Request.RawUrl, out num))
+                    Hits[app.Request.RawUrl] = 1;
+                else
+                    Hits[app.Request.RawUrl] += 1;
+                app.Context.Response.AddHeader("Hits", Hits[app.Request.RawUrl].ToString());
+            }
+            if (_eventHandler != null)
+                _eventHandler(this, null);
+        }
+    }
+}
